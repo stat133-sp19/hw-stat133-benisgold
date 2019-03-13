defab9e238cff273c50606c5034a54880547732b
Ben Goldberg <bgoldberg@berkeley.edu>
2019-03-13 15:24:25 -0700

    up to 4.1

---
+++ code/make-shot-charts-script.R
@@ -0,0 +1,37 @@
+# Title: 
+# Description: 
+# Inputs: 
+# Outputs: 
+
+library(ggplot2)
+library(grid)
+library(jpeg)
+
+curry = read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE)
+thompson = read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE)
+durant = read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE)
+green = read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE)
+iguodala = read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE)
+
+court_file = "../images/nba-court.jpg"
+court_image = rasterGrob(readJPEG(court_file), width = unit(1, "npc"), height = unit(1, "npc"))
+
+curry_shot_chart = ggplot(data = curry) + annotation_custom(court_image, -250, 250, -50, 420) + geom_point(aes(x = x, y = y, color = shot_made_flag)) + ylim(-50, 420) +
+  ggtitle('Shot Chart: Stephen Curry (2016 season)') + theme_minimal()
+ggsave('../images/stephen-curry-shot-chart.pdf', device="pdf", width=6.5, height=5)
+
+thompson_shot_chart = ggplot(data = thompson) + annotation_custom(court_image, -250, 250, -50, 420) + geom_point(aes(x = x, y = y, color = shot_made_flag)) + ylim(-50, 420) +
+  ggtitle('Shot Chart: Klay Thompson (2016 season)') + theme_minimal()
+ggsave('../images/klay-thompson-shot-chart.pdf', device="pdf", width=6.5, height=5)
+
+durant_shot_chart = ggplot(data = durant) + annotation_custom(court_image, -250, 250, -50, 420) + geom_point(aes(x = x, y = y, color = shot_made_flag)) + ylim(-50, 420) +
+  ggtitle('Shot Chart: Kevin Durant (2016 season)') + theme_minimal()
+ggsave('../images/kevin-durant-shot-chart.pdf', device="pdf", width=6.5, height=5)
+
+green_shot_chart = ggplot(data = green) + annotation_custom(court_image, -250, 250, -50, 420) + geom_point(aes(x = x, y = y, color = shot_made_flag)) + ylim(-50, 420) +
+  ggtitle('Shot Chart: Draymond Green (2016 season)') + theme_minimal()
+ggsave('../images/draymond-green-shot-chart.pdf', device="pdf", width=6.5, height=5)
+
+iguodala_shot_chart = ggplot(data = iguodala) + annotation_custom(court_image, -250, 250, -50, 420) + geom_point(aes(x = x, y = y, color = shot_made_flag)) + ylim(-50, 420) +
+  ggtitle('Shot Chart: Andre Iguodala (2016 season)') + theme_minimal()
+ggsave('../images/andre-iguodala-shot-chart.pdf', device="pdf", width=6.5, height=5)
---
+++ code/make-shots-data-script.R
@@ -0,0 +1,57 @@
+# Title:
+# Description
+# Inputs:
+# Outputs:
+
+curry = read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE)
+thompson = read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE)
+durant = read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE)
+green = read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE)
+iguodala = read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE)
+
+curry$name = "Stephen Curry"
+thompson$name = "Klay Thompson"
+durant$name = "Kevin Durant"
+green$name = "Draymond Green"
+iguodala$name = "Andre Iguodala"
+
+curry$shot_made_flag[curry$shot_made_flag == "y"] = "shot_yes"
+curry$shot_made_flag[curry$shot_made_flag == "n"] = "shot_no"
+thompson$shot_made_flag[thompson$shot_made_flag == "y"] = "shot_yes"
+thompson$shot_made_flag[thompson$shot_made_flag == "n"] = "shot_no"
+durant$shot_made_flag[durant$shot_made_flag == "y"] = "shot_yes"
+durant$shot_made_flag[durant$shot_made_flag == "n"] = "shot_no"
+green$shot_made_flag[green$shot_made_flag == "y"] = "shot_yes"
+green$shot_made_flag[green$shot_made_flag == "n"] = "shot_no"
+iguodala$shot_made_flag[iguodala$shot_made_flag == "y"] = "shot_yes"
+iguodala$shot_made_flag[iguodala$shot_made_flag == "n"] = "shot_no"
+
+curry$minute = 15 - curry$minutes_remaining + 15 * (curry$period - 1)
+thompson$minute = 15 - thompson$minutes_remaining + 15 * (thompson$period - 1)
+durant$minute = 15 - durant$minutes_remaining + 15 * (durant$period - 1)
+green$minute = 15 - green$minutes_remaining + 15 * (green$period - 1)
+iguodala$minute = 15 - iguodala$minutes_remaining + 15 * (iguodala$period - 1)
+
+sink('../output/stephen-curry-summary.txt')
+summary(curry)
+sink()
+sink('../output/klay-thompson-summary.txt')
+summary(thompson)
+sink()
+sink('../output/kevin-durant-summary.txt')
+summary(durant)
+sink()
+sink('../output/draymond-green-summary.txt')
+summary(green)
+sink()
+sink('../output/andre-iguodala-summary.txt')
+summary(iguodala)
+sink()
+
+shots_data = rbind(curry, thompson, durant, green, iguodala)
+
+write_csv(shots_data, '../data/shots-data.csv')
+
+sink('../output/shots-data-summary.txt')
+summary(shots_data)
+sink()
